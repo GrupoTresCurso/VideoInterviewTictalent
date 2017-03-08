8047a4bf4e9a896d8ed2125e9cdc989e8e7b404c
grupotrescurso <curso2017>
2017-03-08 12:14:52 +0100

    Actualizado BaseBusiness

--- src/main/java/model/business/BaseBusiness.java
+++ src/main/java/model/business/BaseBusiness.java
@@ -1,4 +1,10 @@
 package model.business;
 
-public interface BaseBusiness {
+import java.util.List;
+
+public interface BaseBusiness<T> {
+    void crearNuevo(T object);
+    T recuperarPorId(int id);
+    List<T> recuperarTodos();
+    void borrarPorId(int id);
 }
