c600173f0acf2298a330340d93ec57e4a4bda3ad
grupotrescurso <curso2017>
2017-03-17 12:31:04 +0100

    Login comenzado e incluido UsuarioBusiness en el controlador

--- src/main/java/controllers/UsuarioController.java
+++ src/main/java/controllers/UsuarioController.java
@@ -1,9 +1,16 @@
 package controllers;
 
+import beans.Usuario;
+import model.business.BaseBusiness;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller("UsuarioController")
 public class UsuarioController implements BaseController {
-    
+
+    @Autowired
+    @Qualifier("UsuarioBusiness")
+    private BaseBusiness<Usuario> usuarioBusiness;
 
 }
