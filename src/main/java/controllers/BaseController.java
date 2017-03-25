ae7b3f9da0bc9512e92aa9c33f3d0ff4c2d426b9
Jopau3112 <jopau3112@gmail.com>
2017-03-25 22:14:27 +0100

    cambiando cosas entrevista

--- src/main/java/controllers/BaseController.java
+++ src/main/java/controllers/BaseController.java
@@ -2,9 +2,6 @@ package controllers;
 
 public interface BaseController {
 
-    String UNA_VISTA_CUALQUIERA = "pruebaindexotro";
-    String UNA_VISTA_CUALQUIERA2 = "pruebaotromas";
-    String UNA_VISTA_VIDEO = "pruebamostrar";
     String CARGAR_VIDEO = "cargar_video";
     String ENTREVISTA_CLONAR="entrevista_clonar";
     String ENTREVISTA_NUEVA="entrevista_nueva";
